bd9cd99285d868260b6e5cbef11b9bb79c595531
Adenisthebestaslan <aden.a.oztas@gmail.com>
2025-06-05 10:53:22 -0500

    new

---
+++ quiz.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+CorrectAwnserQuiz=0
+echo "what is berdly's favorite game?:"
+
+read q1a
+echo $q1a
+
+if [[ "$q1a" == "Dragon Blazers III" ]]; then
+   echo "correct!"
+   ((CorrectAwnserQuiz++))
+fi
+echo "Correct answer: $CorrectAwnserQuiz"
+echo "whois his best freind"
+
+read q2a
+echo $q2a
+
+if [[ "$q2a" == "Noelle" ]]; then
+   echo "correct!"
+   ((CorrectAwnserQuiz++))
+fi
+
+echo "where does he live"
+
+read q3a
+echo $q3a
+
+if [[ "$q3a" == "Hometown" ]]; then
+   echo "correct!"
+   ((CorrectAwnserQuiz++))
+fi
+
+echo "ẉ̷̢̡̮̙̣̞̱̯̺͈̞͎̤̻̌̐͗̋̔͝h̸̡̼̩͎͈̮̩̞̰̼͇̘͎̑͊̇̅̑̈́̐̑̊͠ơ̷̡̨̛͙̪̝̞͍͕͍̥̎̉͋̔̀̆̿̈̀͐ͅ ̸͉̙́̾͑̀̅̅͘f̸͒̋̈́͜r̶̖̙̪̔̃ȩ̶̧̓͛̐̀͊͋̈́̒͒̚͝͝͠ͅz̵͙̰̰̈́͌͗͗̎́͊̚é̴̢̢̢̧̠͕̩͕̦̺̑̃̆̈́̆͑͗̚̕͘e̶͙̮̮̅s̷̺̟̏̂͆̔̓͑̆̍͘ ̸̢̳̔̽̌̀̄̚̚ẖ̵̨͓̞̯̱͔̀́͠i̴̗̦̋̽̋̊m̷̛̤͕̥̣̺̬͌̾̐͒̉͐͊͗͋̂̾̕͘͠ͅ ̸̛͈̝͖͕̲̈́t̷̲̱̗̔́̒̆͂͐̐͘ő̴̧̰̙͎͍̠̤̞̫̞̥̺̊͛̎́̐̆́͘̕͠ ̶̱̅͗̉̏͊̔̀̚͘͠d̵͇̔̿́̄̔͗͐͘͘͜e̶̯̠̝͍̬̠͎͔̤͛͑̾̆̕ȃ̵̩̗̘͍̖̻͉̇́̇̀͌̽͑̕̕t̷̟͚̞̯͈͇͎̻͙̻͉̯̳̭̠͐̈̈́̿̈́̒͒̌͝͠h̸̨̜̱̯̰͕͌́͆̌͂̋̾̓ ̸̧̪̝͆̎̈́̀͊̾̈́̀̓͠ǐ̷͚̮̼̣̙̇̂͜ň̸̢̧̘̟͍͕̻͍͙̙̹͙̓͒̃̚ͅ ̸̧̢͍̝͌̑̅ṱ̵̢̧̲̻̦̼̰̓͊̓́͌̓̓͒͒͂̆̕͠h̵̨̭̹̦̲̟͇̝̯̪̫̀̋́̈́̄̈́̈́̚͜͝ë̷͚̭͎̘͔̳͉̬̗́͜͠ ̴̰͖͊̕s̴̡͕̥͎̲̯̜̝̠͂͜ņ̵̰̗̺̌̓́͜͠o̶̢̢̰̤̯̮͙̫̩̙̱̗͓͔̬̔̆̀w̷̮̪̌͋͐̽͆͋͝g̴̢̘̟̱̖̼͔̗͖͚̻͉̃ͅr̷͉͙̞̦̹͈̥̝͕̺̺͇̪̐͆̑̿̋͂͜ͅa̷̢̰̩̳͋͆͋̚͝v̶̟̠̫͙̫̱̞͒͗e̶̖͇̮̙̹̱̺͔͇͆͆̿̿̾͝ ̵͕̟̜̜͒̑̈́͘r̴̨̨͇̝͕͇͍͍̓͝ọ̶̢̡͔͍͇̪̤̭̎͛̍̈̅̇͂̃̀̍̋̕ù̷͙̙̩͜ẗ̷̢̜̬̟͔̜̩͍͔̲̪́͑̓̾̐͋̾̂̄̍͘e̶̢̡̮̞̠̤̯̺͒͝͝"
+
+read q4a
+echo $q4a
+
+if [[ "$q4a" == "Noelle" ]]; then
+   echo "correct!"
+   ((CorrectAwnserQuiz++))
+fi
+echo  "Correct answers: $CorrectAwnserQuiz / 4"
+echo  "out of 4"
+
+
